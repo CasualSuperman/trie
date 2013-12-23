1290316a78fa630d070c2367334ab8988f39c884
Robert Wertman <robert.wertman@gmail.com>
2013-12-23 04:13:56 -0500

    Still not happy with Search and Remove.
    They could be simplified, or at least made to look more elegant.

--- trie.go
+++ trie.go
@@ -9,6 +9,250 @@ type Trie interface {
 	Update(string, interface{}) error
 }
 
+type trie struct {
+	children  []branch
+	value     interface{}
+	validLeaf bool
+}
+
+type branch struct {
+	letter byte
+	branch *trie
+}
+
+type stackNode struct {
+	index int
+	leaf  *trie
+}
+
+type traversalStack []stackNode
+
+// Iter returns a fully iterative implementation of a Trie, which is faster and uses less stack space.
+func New() *trie {
+	return &trie{nil, nil, false}
+}
+
+func (t *trie) getOrAddChildBranch(r byte) *trie {
+	for _, child := range t.children {
+		if child.letter == r {
+			return child.branch
+		}
+	}
+
+	branch := branch{r, New()}
+	t.children = append(t.children, branch)
+	return branch.branch
+}
+
+func (t *trie) getChild(r byte) int {
+	for i, child := range t.children {
+		if child.letter == r {
+			return i
+		}
+	}
+	return -1
+}
+
+func (t *trie) getChildBranch(r byte) *trie {
+	for _, child := range t.children {
+		if child.letter == r {
+			return child.branch
+		}
+	}
+	return nil
+}
+
+func (t *trie) removeChildIndex(i int) {
+	last := len(t.children)-1
+	if i < last {
+		t.children[i], t.children[last] = t.children[last], t.children[i]
+	}
+	t.children = t.children[:last]
+}
+
+// Add an element to the Trie, mapped to the given value.
+func (t *trie) Add(key string, val interface{}) error {
+	if key == "" {
+		return emptyKey()
+	}
+
+	root := t
+
+	for len(key) > 0 {
+		root = root.getOrAddChildBranch(key[0])
+		key = key[1:]
+	}
+
+	if root.validLeaf {
+		return duplicateKey(key)
+	}
+
+	root.validLeaf = true
+	root.value = val
+
+	return nil
+}
+
+// Get a value from the Trie.
+// Uses a comma ok format.
+func (t *trie) Get(key string) (interface{}, bool) {
+	if key == "" {
+		return nil, false
+	}
+
+	root := t
+
+	for len(key) > 0 {
+		root = root.getChildBranch(key[0])
+		key = key[1:]
+
+		if root == nil {
+			return nil, false
+		}
+	}
+
+	if root.validLeaf {
+		return root.value, true
+	}
+
+	return nil, false
+}
+
+// Search the Trie for all keys starting with the key.
+// A full listing of the Trie is possible using t.Search("")
+func (t *trie) Search(key string) []interface{} {
+	var inlineStack [16]stackNode
+	var results []interface{}
+
+	root := t
+
+	for len(key) > 0 {
+		root = root.getChildBranch(key[0])
+		key = key[1:]
+		if root == nil {
+			return results
+		}
+	}
+
+	tip := 0
+	// The first item on the stack is the last character of our key.
+	inlineStack[0] = stackNode{-1, root}
+	branch := traversalStack(inlineStack[0:1])
+
+	// To help visualize, this is a depth-first search.
+	for tip >= 0 {
+		// Move on to the next sibling of the last leaf we processed.
+		branch[tip].index++
+
+		// Check to see if we're out of children.
+		if branch[tip].index >= len(branch[tip].leaf.children) {
+			// We are, so add ourselves if we're a valid leaf.
+			if branch[tip].leaf.validLeaf {
+				results = append(results, branch[tip].leaf.value)
+			}
+			// This branch is completely done; remove it from the stack.
+			branch = branch[:tip]
+			tip--
+			continue
+		}
+
+		// Not out of children, push the next one onto the stack.
+		branch = append(branch, stackNode{
+			// We start at -1 because the first thing we do is increment this.
+			-1,
+			branch[tip].leaf.children[branch[tip].index].branch,
+		})
+		tip++
+	}
+	return results
+}
+
+// Remove the key from the Trie.
+// The Trie will compact itself if possible.
+func (t *trie) Remove(key string) error {
+	var inlineStack [16]stackNode
+	tip := -1
+	branch := traversalStack(inlineStack[0:0])
+	root := t
+
+	for len(key) > 0 {
+		i := root.getChild(key[0])
+
+		if i == -1 {
+			return notFound(key)
+		}
+
+		branch = append(branch, stackNode{
+			i,
+			root,
+		})
+
+		root = root.children[i].branch
+		key = key[1:]
+		tip++
+	}
+
+	branch = append(branch, stackNode{
+		0,
+		root,
+	})
+	tip++
+
+	if !branch[tip].leaf.validLeaf {
+		return notFound(key)
+	}
+
+	branch[tip].leaf.value = nil
+	branch[tip].leaf.validLeaf = false
+
+	// Chop off our dead branches.
+	for tip > 0 {
+		// If this branch isn't a valid leaf and has no children.
+		if !branch[tip].leaf.validLeaf && len(branch[tip].leaf.children) == 0 {
+			// Find our parent.
+			trim := branch[tip-1]
+			// Remove us from our parent. (quickly if possible)
+			if len(trim.leaf.children) == 1 {
+				trim.leaf.children = nil
+			} else {
+				trim.leaf.removeChildIndex(trim.index)
+			}
+			// Become our parent and repeat this process.
+			tip--
+		} else {
+			break
+		}
+	}
+
+	return nil
+}
+
+// Update the value of an existing element in the trie.
+func (t *trie) Update(key string, val interface{}) error {
+	if key == "" {
+		return emptyKey()
+	}
+
+	root := t
+
+	for len(key) > 0 {
+		root = root.getChildBranch(key[0])
+		key = key[1:]
+		
+		if root == nil {
+			return notFound(key)
+		}
+	}
+
+	if !root.validLeaf {
+		return notFound(key)
+	}
+
+	root.value = val
+
+	return nil
+}
+
 type (
 	emptyKeyError bool
 	keynotFoundError string
@@ -37,225 +281,4 @@ func (err keynotFoundError) Error() string {
 
 func (err duplicateKeyError) Error() string {
 	return "key already in trie: '" + string(err) + "'"
-}
-
-type trie struct {
-	children  []branch
-	value     interface{}
-	validLeaf bool
-}
-
-type branch struct {
-	letter byte
-	branch *trie
-}
-
-type stackNode struct {
-	index int
-	leaf  *trie
-}
-
-// Iter returns a fully iterative implementation of a Trie, which is faster and uses less stack space.
-func New() Trie {
-	return &trie{nil, nil, false}
-}
-
-func (t *trie) getChild(r byte) int {
-	for i, child := range t.children {
-		if child.letter == r {
-			return i
-		}
-	}
-	return -1
-}
-
-// Add an element to the Trie, mapped to the given value.
-func (t *trie) Add(key string, val interface{}) error {
-	if key == "" {
-		return emptyKey()
-	}
-
-	root := t
-
-	for len(key) > 0 {
-		i := root.getChild(key[0])
-
-		if i == -1 {
-			branch := branch{
-				key[0],
-				&trie{
-					nil,
-					nil,
-					false,
-				},
-			}
-			root.children = append(root.children, branch)
-			root = branch.branch
-		} else {
-			root = root.children[i].branch
-		}
-
-		key = key[1:]
-	}
-
-	if root.validLeaf {
-		return duplicateKey(key)
-	}
-
-	root.validLeaf = true
-	root.value = val
-
-	return nil
-}
-
-// Get a value from the Trie.
-// Uses a comma ok format.
-func (t *trie) Get(key string) (interface{}, bool) {
-	if key == "" {
-		return nil, false
-	}
-
-	root := t
-
-	for len(key) > 0 {
-		i := root.getChild(key[0])
-
-		if i == -1 {
-			return nil, false
-		}
-
-		root = root.children[i].branch
-		key = key[1:]
-	}
-
-	if root.validLeaf {
-		return root.value, true
-	}
-
-	return nil, false
-}
-
-// Search the Trie for all keys starting with the key.
-// A full listing of the Trie is possible using t.Search("")
-func (t *trie) Search(key string) []interface{} {
-	root := t
-	branch := make([]stackNode, 1, 32)
-	results := make([]interface{}, 0)
-
-	for len(key) > 0 {
-		next := root.getChild(key[0])
-		if next == -1 {
-			return results
-		}
-		root = root.children[next].branch
-		key = key[1:]
-	}
-
-	branch[0] = stackNode{-1, root}
-	tip := 0
-
-	for tip >= 0 {
-		branch[tip].index++
-		if branch[tip].index >= len(branch[tip].leaf.children) {
-			if branch[tip].leaf.validLeaf {
-				results = append(results, branch[tip].leaf.value)
-			}
-			branch = branch[:tip]
-			tip--
-			continue
-		}
-
-		next := branch[tip].leaf
-		branch = append(branch, stackNode{
-			-1,
-			next.children[branch[tip].index].branch,
-		})
-		tip++
-	}
-	return results
-}
-
-// Remove the key from the Trie.
-// The Trie will compact itself if possible.
-func (t *trie) Remove(key string) error {
-	tip := -1
-	branch := make([]stackNode, 0, 32)
-	root := t
-
-	for len(key) > 0 {
-		i := root.getChild(key[0])
-
-		if i == -1 {
-			return notFound(key)
-		}
-
-		branch = append(branch, stackNode{
-			i,
-			root,
-		})
-
-		root = root.children[i].branch
-		key = key[1:]
-		tip++
-
-		if len(key) == 0 {
-			branch = append(branch, stackNode{
-				0,
-				root,
-			})
-			tip++
-		}
-	}
-
-	if branch[tip].leaf.validLeaf {
-		branch[tip].leaf.value = nil
-		branch[tip].leaf.validLeaf = false
-	} else {
-		return notFound(key)
-	}
-
-	for tip > 0 {
-		if !branch[tip].leaf.validLeaf && len(branch[tip].leaf.children) == 0 {
-			trim := branch[tip-1]
-			trim.leaf.children[trim.index] = trim.leaf.children[len(trim.leaf.children)-1]
-			trim.leaf.children[len(trim.leaf.children)-1].branch = nil
-			trim.leaf.children = trim.leaf.children[:len(trim.leaf.children)-1]
-			if len(trim.leaf.children) == 0 {
-				trim.leaf.children = nil
-			}
-			tip--
-		} else {
-			break
-		}
-	}
-
-	return nil
-}
-
-// Update the value of an existing element in the trie.
-func (t *trie) Update(key string, val interface{}) error {
-	if key == "" {
-		return emptyKey()
-	}
-
-	root := t
-
-	for len(key) > 0 {
-		i := root.getChild(key[0])
-		
-		if i == -1 {
-			return notFound(key)
-		}
-
-		root = root.children[i].branch
-		key = key[1:]
-	}
-
-	if !root.validLeaf {
-		return notFound(key)
-	}
-
-	root.value = val
-
-	return nil
 }
